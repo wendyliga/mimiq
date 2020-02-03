f4353fd24b6205ae15eaeba54fad949cd6879c37
Wendy Liga <agilydnew@gmail.com>
2020-02-03 13:16:49 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mimiq
+
+mimiq is simple executable to record your Xcode simulator and convert it to GIF.
---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if ! [ -f "mimiq.script" ]; then
+    echo "missing mimiq file" && exit
+fi
+
+rm -f mimiq && cp mimiq.script mimiq && chmod +x mimiq
