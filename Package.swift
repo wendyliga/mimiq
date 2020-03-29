abc7cea7b94e3d9a39240dc29a727599f2222d7c
Wendy Liga <agilydnew@gmail.com>
2020-03-29 18:56:14 +0700

    rewrite to swift

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "mimiq",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.0.1")),
+        .package(url: "https://github.com/wendyliga/ConsoleIO.git", .branch("improve_input")),
+        .package(url: "https://github.com/wendyliga/Explorer.git", from: "0.0.3"),
+    ],
+    targets: [
+        .target(
+            name: "mimiq",
+            dependencies: ["ArgumentParser", "Explorer", "ConsoleIO"]
+        ),
+        .testTarget(
+            name: "mimiqTests",
+            dependencies: ["mimiq"]),
+    ]
+)
