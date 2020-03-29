df920bbc9813bc32b8f9e3aa1e84a4c4f9551a12
Wendy Liga <agilydnew@gmail.com>
2020-03-29 22:12:22 +0700

    add version

--- Sources/mimiq/main.swift
+++ Sources/mimiq/main.swift
@@ -1,3 +1,27 @@
+/**
+ MIT License
+
+ Copyright (c) 2020 Wendy Liga
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
 import ArgumentParser
 import ConsoleIO
 import Explorer
@@ -6,7 +30,7 @@ import Foundation
 // MARK: - Configuration
 
 private let appName = "mimiq"
-private let version = "0.0.1"
+private let version = "0.3.0"
 private let defaultResultPath = "~/Desktop/"
 private let tempFolder = "~/"
 
@@ -198,14 +222,36 @@ struct List: ParsableCommand {
     }
 }
 
+struct Version: ParsableCommand {
+    static var configuration = CommandConfiguration(
+      commandName: "version",
+      abstract: "\(appName) version",
+      discussion: "",
+      helpNames: .long
+    )
+    
+    func run() throws {
+        print("current version \(version)")
+    }
+}
+
 struct Mimiq: ParsableCommand {
     init() {}
     
     static var configuration = CommandConfiguration(
       commandName: "",
-      abstract: "Record your Xcode simulator and convert it to GIF",
-      discussion: "",
-      subcommands: [List.self],
+      abstract:
+        """
+        Record your Xcode simulator and convert it to GIF
+        """,
+      discussion:
+        """
+        \(appName) \(version)
+        
+        Created by Wendy Liga
+        Learn more https://github.com/wendyliga/mimiq
+        """,
+      subcommands: [List.self, Version.self],
       helpNames: .long
     )
     
