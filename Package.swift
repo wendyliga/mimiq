b41dc66a894b481cd183d48f3324ec58c19ab9ca
Wendy Liga <agilydnew@gmail.com>
2020-06-15 06:17:47 +0700

    add product on package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "mimiq",
+    products: [
+        .executable(name: "mimiq", targets: ["mimiq"])
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.0.1")),
         .package(url: "https://github.com/wendyliga/ConsoleIO.git", .upToNextMajor(from: "1.0.0")),
