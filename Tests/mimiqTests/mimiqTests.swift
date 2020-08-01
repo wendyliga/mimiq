e3b9f3cae44cf344cc53af197374ff1221b1d225
Wendy Liga <agilydnew@gmail.com>
2020-08-01 19:28:13 +0700

    bump version

--- Tests/mimiqTests/mimiqTests.swift
+++ Tests/mimiqTests/mimiqTests.swift
@@ -106,7 +106,7 @@ final class mimiqTests: XCTestCase {
     }
     
     func test_checkVersion() throws {
-        let expected = "current version 0.4.0\n"
+        let expected = "current version 0.4.1\n"
         
         XCTAssertEqual(try shellProcess(args: ["version"]), expected)
     }
